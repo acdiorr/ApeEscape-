afd6a2c55bd95fe665cd1e5083149fc97f05eea7
Ricardo <ricardojp2511@gmail.com>
2020-12-05 18:37:16 -0500

    Zombie Anims and Movement
    Zombie anims work now! Also, zombies don't attack or anything. Can we please just make it so that when your hit you take damage, none of this turnspeed, lunge at you crap

--- SuperSpecialAwesomeMonkeyZombieSim/Monkkers/SpriteLib3.0-v2.0/Zombie.cpp
+++ SuperSpecialAwesomeMonkeyZombieSim/Monkkers/SpriteLib3.0-v2.0/Zombie.cpp
@@ -1,6 +1,63 @@
 #include "Zombie.h"
 #include "Utilities.h"
 
+Zombie::Zombie()
+{
+}
+
+Zombie::Zombie(std::string& fileName, const std::string& animationJSON, int width, int height, Sprite* sprite, AnimationController* controller, Transform* transform, PhysicsBody* body)
+{
+    InitZombie(fileName, animationJSON, width, height, sprite, controller, transform, body);
+}
+
+void Zombie::InitZombie(std::string& fileName, const std::string& animationJSON, int width, int height, Sprite* sprite, AnimationController* controller, Transform* transform, PhysicsBody* body)
+{
+    //Store references to the components
+    m_sprite = sprite;
+    m_animController = controller;
+    m_transform = transform;
+    m_physBody = body;
+
+    //Initialize UVs
+    m_animController->InitUVs(fileName);
+
+    //Loads the texture and sets width and height
+    m_sprite->LoadSprite(fileName, width, height, true, m_animController);
+    m_animController->SetVAO(m_sprite->GetVAO());
+    m_animController->SetTextureSize(m_sprite->GetTextureSize());
+
+    //Loads in the animations json file
+    nlohmann::json animations = File::LoadJSON(animationJSON);
+    std::string animationNames[2] = { "idle", "moving" };
+
+    int length = sizeof(animationNames) / sizeof(animationNames[0]);
+    for (int i = 0; i < length; i++)
+    {
+        m_animController->AddAnimation(animations[animationNames[i]]);
+    }
+
+    //Set Default Animation
+    m_animController->SetActiveAnim(0);
+}
+
+void Zombie::AnimationUpdate()
+{
+    int activeAnimation = 0;
+
+    if (m_moving)
+    {
+        //Puts it into the WALK category
+        activeAnimation = (int)ZombAnimEnums::Moving;
+    }
+
+    else
+    {
+        activeAnimation = (int)ZombAnimEnums::Standing;
+    }
+
+    //SetActiveAnimation(activeAnimation + (int)m_facing);
+    SetActiveAnimation(activeAnimation);
+}
 
 float Zombie::getMovementSpeed()
 {
@@ -18,8 +75,8 @@ void Zombie::dealDamage(float damageAmount)
 }
 
 void Zombie::zombieUpdate(PhysicsBody ZombiePhysicsBody, std::vector <unsigned int>* zEnts, int Zentity)
-//std::cout << "X: " << ZombiePhysicsBody.GetPosition().x << " Y: " << ZombiePhysicsBody.GetPosition().y << std::endl;
 {
+    vec3 movement = vec3(0, 0, 0);
     if (this->timeSinceLastAttack > 0) {
         this->timeSinceLastAttack -= Timer::deltaTime;
         std::cout << "Zombie Attacked and cannot move rn" << std::endl;
@@ -52,7 +109,17 @@ void Zombie::zombieUpdate(PhysicsBody ZombiePhysicsBody, std::vector <unsigned i
         //Move in the direction the zombie is facing
         {
             float angle = ZombiePhysicsBody.GetRotationAngleDeg();
-            b2Vec2(float32(this->movementSpeed * sin(-angle* 0.01745329f)), float32(this->movementSpeed * cos(-angle * 0.01745329f)));
+            movement = vec3(movementSpeed * sin(-angle* 0.01745329f), movementSpeed * cos(-angle * 0.01745329f), 0);
+            m_physBody->SetVelocity(movement);
+            if (m_physBody->GetBody()->GetLinearVelocity() == b2Vec2(0.f, 0.f))
+            {
+                m_moving = false;
+            }
+            else
+            {
+                m_moving = true;
+            }
+
         }
         //Attack Player if within range
         {
@@ -86,14 +153,12 @@ void Zombie::zombieUpdate(PhysicsBody ZombiePhysicsBody, std::vector <unsigned i
             }
         }
     }
+    AnimationUpdate();
     if (this->health < 1) {
         killZombie(zEnts, Zentity);
     }
 }
 
-void Zombie::makeZombie()
-{
-}
 
 void Zombie::killZombie(std::vector <unsigned int> *zEnts, int entity)
 {
@@ -103,4 +168,19 @@ void Zombie::killZombie(std::vector <unsigned int> *zEnts, int entity)
         }
     }
     PhysicsBody::m_bodiesToDelete.push_back(entity);
+}
+
+void Zombie::AttachBody(PhysicsBody* body)
+{
+    m_physBody = body;
+}
+
+void Zombie::AttachAnimation(AnimationController* controller)
+{
+    m_animController = controller;
+}
+
+void Zombie::SetActiveAnimation(int anim)
+{
+    m_animController->SetActiveAnim(anim);
 }
